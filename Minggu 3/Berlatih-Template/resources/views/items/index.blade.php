e52720d0309787f8b0c96d061983203436d9778e
rezalastana <rezalastana21@gmail.com>
2020-07-02 08:52:07 +0700

    Blade Template

---
+++ Minggu 3/Berlatih-Template/resources/views/items/index.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte.master')
+
+@section('content')
+    @include('items.partials.isi-index')
+@endsection
+
+@push('scripts')
+<script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.js')}}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
+<script>
+    $(function () {
+        $("#example1").DataTable();
+    });
+</script>
+@endpush
